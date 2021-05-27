0771aff715b7ed3961536d863f882153f9ba579d
Harvey <harvey0379@163.com>
2021-05-27 13:19:12 +0800

    =

--- src/index.js
+++ src/index.js
@@ -1,26 +1,8 @@
-import call from "./mine/call";
-import apply from "./mine/apply";
-import bind from "./mine/bind";
+let b = false;
 
-const add = function (a, b) {
-  console.log(a, b, this);
-  return a + b;
-};
+console.log((b = true));
 
-const obj = {
-  name: "obj",
-};
+console.log(b);
 
-const param = [2, 4];
-
-// const res = call(add, obj, ...param);
-// const res4 = call(add, null, ...param);
-// const res1 = apply(add, obj, param);
-
-// console.log("res", res);
-// console.log("res1", res1);
-// console.log("res4", res4);
-// console.log(bind(add, obj)(...param));
-// console.log(bind(add, obj, 100));
-console.log(bind(add, obj, 100)(...param));
-console.log(obj);
+console.log((b = false));
+console.log(b);
