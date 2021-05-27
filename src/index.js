699c777a62cefa60e02ad366e69b8564b9de8b7a
Harvey <harvey0379@163.com>
2021-05-27 12:05:39 +0800

    my call apply bind

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import demo from "./demo/requestAnimationFrame";
+import call from "./mine/call";
+import apply from "./mine/apply";
+import bind from "./mine/bind";
 
-demo();
+const add = function (a, b) {
+  console.log(a, b, this);
+  return a + b;
+};
+
+const obj = {
+  name: "obj",
+};
+
+const param = [2, 4];
+
+// const res = call(add, obj, ...param);
+// const res4 = call(add, null, ...param);
+// const res1 = apply(add, obj, param);
+
+// console.log("res", res);
+// console.log("res1", res1);
+// console.log("res4", res4);
+// console.log(bind(add, obj)(...param));
+// console.log(bind(add, obj, 100));
+console.log(bind(add, obj, 100)(...param));
+console.log(obj);
