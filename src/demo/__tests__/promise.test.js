a13dfd9bdb4b2122ea57aefc1dfbd10754f34123
Harvey <harvey0379@163.com>
2021-06-24 15:12:49 +0800

    test:promise

---
+++ src/demo/__tests__/promise.test.js
@@ -0,0 +1,27 @@
+const demo = function (a, b) {
+  return new Promise((resolve, reject) => {
+    console.log(111);
+    if (a > b) {
+      reject("some thing error here");
+      console.log(222);
+    }
+    resolve("success");
+    console.log(333);
+  });
+};
+
+test("test success", async () => {
+  const res = await demo(1, 2);
+
+  expect(res).toBe("success");
+});
+
+test("test error", async () => {
+  try {
+    const res = await demo(2, 1);
+
+    expect(res).toBe("success");
+  } catch (error) {
+    console.error(`The error is [${error}]`);
+  }
+});
