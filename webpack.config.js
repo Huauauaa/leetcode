b82784aed39c65705c85d850f9d481bfcf803885
huaweiwei <harvey0379@163.com>
2022-02-27 22:13:40 +0800

    fix: Missing class properties transform

--- webpack.config.js
+++ webpack.config.js
@@ -13,4 +13,14 @@ module.exports = {
     compress: true,
     port: 9000,
   },
+  loaders: [
+    {
+      test: /\.js?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: [
+        "react-hot",
+        "babel?presets[]=react,presets[]=es2015,presets[]=stage-0",
+      ],
+    },
+  ],
 };
