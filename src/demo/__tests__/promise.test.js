2db4c14e3c9fd9550dfbc4580d124396dc327dc0
Harvey <harvey0379@163.com>
2023-03-25 07:49:19 +0800

    promise

--- src/demo/__tests__/promise.test.js
+++ src/demo/__tests__/promise.test.js
@@ -2,26 +2,40 @@ const demo = function (a, b) {
   return new Promise((resolve, reject) => {
     console.log(111);
     if (a > b) {
-      reject("some thing error here");
+      reject('some thing error here');
       console.log(222);
+    } else if (a === b) {
+      throw 1;
     }
-    resolve("success");
+    resolve('success');
     console.log(333);
   });
 };
 
-test("test success", async () => {
-  const res = await demo(1, 2);
+test('test success', async () => {
+  const res = await demo(1, 2); // 111 333
 
-  expect(res).toBe("success");
+  expect(res).toBe('success');
 });
 
-test("test error", async () => {
+test('test error', async () => {
   try {
-    const res = await demo(2, 1);
+    const res = await demo(2, 1); // 111 222 333
 
-    expect(res).toBe("success");
+    expect(res).toBe('success');
+    console.log(444);
   } catch (error) {
-    console.log(`The error is [${error}]`);
+    console.log(`The error is [${error}]`); // The error is [some thing error here]
+  }
+});
+
+test('test throw', async () => {
+  try {
+    const res = await demo(2, 2); // 111
+
+    expect(res).toBe('success');
+    console.log(444);
+  } catch (error) {
+    console.log(`The error is [${error}]`); // The error is [1]
   }
 });
