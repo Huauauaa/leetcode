5ef7ff5bc85a091279fca6107160b0c939458b4a
Harvey <harvey0379@163.com>
2024-07-17 23:54:18 +0800

    [go]two-sum

--- src/leetcode/00001.two-sum/1.go
+++ src/leetcode/00001.two-sum/1.go
@@ -3,9 +3,16 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-    return nums
+    for i, x := range nums {
+        for j := i + 1; j < len(nums); j++ {
+            if x+nums[j] == target {
+                return []int{i, j}
+            }
+        }
+    }
+    return nil
 }
 
 func main() {
-    fmt.Print(twoSum([]int{1, 2, 3}, 9))
+    fmt.Print(twoSum([]int{2, 7, 11, 15}, 9))
 }
