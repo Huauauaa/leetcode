f4c5e48dc3059d1b79d96e2cf3f675d958ac20a5
Harvey <harvey0379@163.com>
2024-08-18 10:10:25 +0800

    [python]median-of-two-sorted-arrays 2

--- src/leetcode/00004.median-of-two-sorted-arrays/4.py
+++ src/leetcode/00004.median-of-two-sorted-arrays/4.py
@@ -1,29 +1,42 @@
 from typing import List
 
 
+# class Solution:
+#     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+#         res = nums1[::]
+
+#         def inset(t):
+#             left = 0
+#             right = len(res) - 1
+#             while left <= right:
+#                 mid = (left + right) >> 1
+#                 if res[mid] > t:
+#                     right = mid - 1
+#                 else:
+#                     left = mid + 1
+#             res.insert(left, t)
+
+#         for i in nums2:
+#             inset(i)
+
+
+#         length = len(res)
+#         if length % 2 == 0:
+#             return (res[length // 2 - 1] + res[length // 2]) / 2
+#         else:
+#             return res[length // 2]
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
-        res = nums1[::]
+        res = []
+        while nums1 and nums2:
+            if nums1[0] >= nums2[0]:
+                res.append(nums2.pop(0))
+            else:
+                res.append(nums1.pop(0))
+        res += nums1 + nums2
 
-        def inset(t):
-            left = 0
-            right = len(res) - 1
-            while left <= right:
-                mid = (left + right) >> 1
-                if res[mid] > t:
-                    right = mid - 1
-                else:
-                    left = mid + 1
-            res.insert(left, t)
-
-        for i in nums2:
-            inset(i)
-
-        length = len(res)
-        if length % 2 == 0:
-            return (res[length // 2 - 1] + res[length // 2]) / 2
-        else:
-            return res[length // 2]
+        mid = len(res) // 2
+        return (res[mid] + res[~mid]) / 2
 
 
 if __name__ == "__main__":
